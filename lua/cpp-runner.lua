995c4d7426326e485ef77c68f655a46c12ee4d0b
Sc1pex <scipex.dev@gmail.com>
2023-10-08 17:29:11 +0300

    Create projects on the server

--- lua/cpp-runner.lua
+++ lua/cpp-runner.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-function project_name()
+local function project_name()
   local name = vim.api.nvim_buf_get_name(0)
   if vim.endswith(name, ".cpp") == false and vim.endswith(name, ".in") == false and vim.endswith(name, ".out") == false then
     return nil
@@ -8,19 +8,48 @@ function project_name()
   return vim.split(name, ".", { plain = true })[1]
 end
 
+local function post_to(path, async)
+  async = async or false
+
+  local opts = {}
+  if async then
+    opts.callback = function(out)
+      if out.status ~= 200 then
+        print("Network error. Is the server running?")
+      end
+    end
+  end
+
+  local curl = require("plenary.curl")
+  curl.post("localhost:42069/" .. path, opts)
+end
+
+local function open_proj(name)
+  local function openfile(path)
+    if vim.fn.filereadable(path) == 1 then
+      vim.cmd("e " .. path)
+    end
+  end
+
+  local path = vim.fn.getcwd() .. "/" .. name .. "/" .. name
+  openfile(path .. ".cpp")
+  openfile(path .. ".in")
+  openfile(path .. ".out")
+end
+
 M.fstream_proj = function()
-  vim.ui.input({ prompt = "Fstream project name" },
+  vim.ui.input({
+      prompt = "Fstream project name",
+    },
     function(input)
       if input == nil then
         return
       end
 
-      local cmd = "proj_gen f " .. input
-      os.execute(cmd)
-      local file = "e " .. input .. "/" .. input
-      vim.cmd(file .. ".in")
-      vim.cmd(file .. ".out")
-      vim.cmd(file .. ".cpp")
+      local post = "create/f/" .. input
+      post_to(post)
+
+      open_proj(input)
     end
   )
 end
@@ -32,10 +61,10 @@ M.istream_proj = function()
         return
       end
 
-      local cmd = "proj_gen i " .. input
-      os.execute(cmd)
-      local file = "e " .. input .. "/" .. input
-      vim.cmd(file .. ".cpp")
+      local post = "create/i/" .. input
+      post_to(post)
+
+      open_proj(input)
     end
   )
 end
@@ -54,6 +83,7 @@ M.close_proj = function()
   end
 end
 
+
 M.open_proj = function()
   vim.ui.input({ prompt = "Project name" },
     function(input)
@@ -61,16 +91,7 @@ M.open_proj = function()
         return
       end
 
-      local function openfile(path)
-        if vim.fn.filereadable(path) == 1 then
-          vim.cmd("e " .. path)
-        end
-      end
-
-      local path = vim.fn.getcwd() .. "/" .. input .. "/" .. input
-      openfile(path .. ".cpp")
-      openfile(path .. ".in")
-      openfile(path .. ".out")
+      open_proj(input)
     end
   )
 end
@@ -83,14 +104,8 @@ M.run_proj = function()
   local name_s = vim.split(name, "/", { plain = true })
   name = name_s[#name_s]
 
-  local curl = require("plenary.curl")
-  local res = curl.post("localhost:42069/run/" .. name)
-  print(vim.inspect(res))
-
-  if res.status ~= 200 then
-    print("Error running project. Is the server running?")
-    print("Error: " .. res.body)
-  end
+  local post = "run/" .. name
+  post_to(post, true)
 end
 
 return M
--- tool/src/main.rs
+++ tool/src/main.rs
@@ -1,21 +1,97 @@
-use std::process::Command;
-
 use axum::extract::Path;
 use axum::routing::post;
 use axum::Router;
+use std::process::{Command, ExitStatus};
+use std::time::Duration;
 
-async fn index(Path(name): Path<String>) {
-    let _ = Command::new("fish")
-        .arg("-c")
-        .arg(format!("~/.local/bin/proj_run {name}"))
+fn compile(proj: String) -> bool {
+    println!("Compiling {proj}");
+
+    let exit_code = Command::new("g++")
+        .args(&[
+            "-std=c++14",
+            "-Wall",
+            "-Wextra",
+            "-o",
+            &proj,
+            &format!("{}.cpp", &proj),
+        ])
+        .current_dir(std::fs::canonicalize(format!("./{}", &proj)).unwrap())
         .stdout(std::process::Stdio::inherit())
         .spawn()
-        .expect("Failed to run project {name}");
+        .expect("Failed to spawn g++")
+        .wait()
+        .expect("Failed to run g++");
+
+    exit_code.success()
+}
+
+fn run(proj: String) -> Result<Duration, ExitStatus> {
+    println!("Running {proj}");
+
+    let mut cmd = Command::new(format!("./{proj}"));
+    cmd.current_dir(std::fs::canonicalize(format!("./{}", &proj)).unwrap());
+    cmd.stdout(std::process::Stdio::inherit());
+
+    let start = std::time::Instant::now();
+    let exit_code = cmd
+        .spawn()
+        .expect(&format!("Failed to spawn {proj}"))
+        .wait()
+        .expect(&format!("Failed to run {proj}"));
+
+    if exit_code.success() {
+        Ok(start.elapsed())
+    } else {
+        Err(exit_code)
+    }
+}
+
+async fn run_handle(Path(name): Path<String>) {
+    if !compile(name.clone()) {
+        println!("Project {name} failed to compile");
+        return;
+    }
+    match run(name.clone()) {
+        Ok(duration) => println!("\nDone in {} ms", duration.as_millis()),
+        Err(code) => println!("\nExited with code {code}"),
+    }
+}
+
+fn create_iproj(name: String) {
+    use std::fs;
+
+    fs::create_dir(&name).expect("Failed to create project directory");
+    fs::File::create(format!("{}/{}.cpp", &name, &name)).expect("Failed to create project file");
+}
+
+fn create_fproj(name: String) {
+    use std::fs;
+
+    fs::create_dir(&name).expect("Failed to create project directory");
+    fs::File::create(format!("{}/{}.cpp", &name, &name)).expect("Failed to create project file");
+    fs::File::create(format!("{}/{}.in", &name, &name)).expect("Failed to create project file");
+    fs::File::create(format!("{}/{}.out", &name, &name)).expect("Failed to create project file");
+}
+
+#[derive(serde::Deserialize)]
+struct Create {
+    name: String,
+    t: String,
+}
+async fn create_handle(Path(Create { name, t }): Path<Create>) {
+    match t.as_str() {
+        "i" => create_iproj(name),
+        "f" => create_fproj(name),
+        _ => println!("Unknown project type {t}"),
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/run/:name", post(index));
+    let app = Router::new()
+        .route("/run/:name", post(run_handle))
+        .route("/create/:t/:name", post(create_handle));
 
     axum::Server::bind(&"0.0.0.0:42069".parse().unwrap())
         .serve(app.into_make_service())
